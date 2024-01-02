a30fc3980b165997101d8b1a2be01f5d6e349e83
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-01-02 07:12:22 +0530

    feat:form-validation
    fixes #1

---
+++ README.md
@@ -0,0 +1,26 @@
+## Courier Pickup App
+Simple courier pickuping app made on vanilla javascript
+
+### Screenshots
+> will be updated shortly!
+
+### TODOs:
+- [x] Form Validation
+- [] Save it to LocalStorage (storage options)
+- [] Show Courier request based on state
+- [] other CRUD operations
+
+#### 01. Form Validation
+- []- Client
+    - Native browser validation
+        - doing validation using html like adding required keyword
+    - External Javascript Libraries to validate your form
+- [] - Server
+    - Code validation on server
+- [] - Database
+    - Columns validation.
+
+#### License : MIT (Do whatever, you want)
+
+### Authors:
+- [Muthu Akalya A] (https://github.com/)
---
+++ asset/js/main.js
@@ -0,0 +1,52 @@
+import JustValidate from "just-validate";
+
+// getting form element
+const formEl = document.getElementById("courier-request-form");
+
+const validateForm = new JustValidate(formEl);
+
+validateForm.addField(
+  "#name",
+  [
+    { rule: "required" },
+    {
+      rule: "minLength",
+      value: 3,
+    },
+    {
+      rule: "maxLength",
+      value: 20,
+    },
+  ],
+  {
+    errorLabelCssClass:["form-error"]}
+  
+);
+validateForm.addField("#number",[
+    {rule:"required"},
+    {
+      rule:"number"
+    },
+    {
+      rule:"minNumber",
+      value:10
+    },
+    {
+      rule:"maxNumber",
+      value:"10"
+    }
+],{
+  errorLabelCssClass:["form-error"]
+})
+validateForm.addField("#pickup-date",[
+  {rule:"required"},
+
+],{
+errorLabelCssClass:["form-error"]
+})
+validateForm.addField("#pickup-area",[
+  {rule:"required"},
+
+],{
+errorLabelCssClass:["form-error"]
+})
